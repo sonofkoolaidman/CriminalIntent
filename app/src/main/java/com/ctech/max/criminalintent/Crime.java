d3a57b3ad157a9c873f641868aaf5d928592aa0b
sonofkoolaidman <35243264+sonofkoolaidman@users.noreply.github.com>
2019-04-03 08:22:58 -0500

    initial commit

---
+++ app/src/main/java/com/ctech/max/criminalintent/Crime.java
@@ -0,0 +1,48 @@
+package com.ctech.max.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+
+    public UUID getId() {
+        return mId;
+    }
+
+    private UUID mId;   //the crime's unique id
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        this.mTitle = mTitle;
+    }
+
+    private String mTitle;  //title of the crime
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        this.mDate = date;
+    }
+
+    private Date mDate;  //date of the crime
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        this.mSolved = solved;
+    }
+
+    private boolean mSolved;  //has the crime been solved?
+
+    public Crime() {
+        mId = UUID.randomUUID(); //automatically generate a random id
+        mDate = new Date(); //defaults the date to right now
+    }
+}
