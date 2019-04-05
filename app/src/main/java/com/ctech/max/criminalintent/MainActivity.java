d572fdc1d7e9c65fb0c8e5834c2f573d452c0df9
sonofkoolaidman <35243264+sonofkoolaidman@users.noreply.github.com>
2019-04-05 09:01:05 -0500

    added the CrimeLab and FragmentManager.

--- app/src/main/java/com/ctech/max/criminalintent/MainActivity.java
+++ app/src/main/java/com/ctech/max/criminalintent/MainActivity.java
@@ -1,5 +1,7 @@
 package com.ctech.max.criminalintent;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment myFragment = fm.findFragmentById(R.id.fragment_container);
+
+        // the first time through, the fragment will be null, so create it
+        if(myFragment == null) {
+            myFragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, myFragment)
+                    .commit();
+        }
     }
 }
