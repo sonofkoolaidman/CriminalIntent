71f7d1d993b2b877cb0b92be1f966f644c19475e
sonofkoolaidman <35243264+sonofkoolaidman@users.noreply.github.com>
2019-04-18 09:04:06 -0500

    halfway to finishing calender

--- app/src/main/java/com/ctech/max/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/ctech/max/criminalintent/DatePickerFragment.java
@@ -8,14 +8,48 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class DatePickerFragment extends DialogFragment {
+
+    //this is the key into the bundle where we will store the date
+    private static final String ARG_DATE = "crime_date";
+
+    private DatePicker mDatePicker;
+
+    //this is the method the CrimeFragment will call to create a DatePicker
+    public static DatePickerFragment newInstance(Date date) {
+
+        //we'll store the data in a bundle
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+
+        DatePickerFragment myFragment = new DatePickerFragment();
+        myFragment.setArguments(args);
+        return myFragment;
+    }
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
 
+        Date myDate = (Date) getArguments().getSerializable(ARG_DATE);
+
+        // create a calender object so we can extract the year, month, and day to initialize the DatePicker
+        Calendar myCalender = Calendar.getInstance();
+        myCalender.setTime(myDate);
+        int year = myCalender.get(Calendar.YEAR);
+        int month = myCalender.get(Calendar.MONTH);
+        int day = myCalender.get(Calendar.DAY_OF_MONTH);
+
         View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+
+        // initialize the datepicker
+        mDatePicker = (DatePicker) v.findViewById(R.id.dialog_date_picker);
+        mDatePicker.init(year, month, day, null);
 
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
