07793e10a26e10a535b789443060c7f02a91ddda
sonofkoolaidman <35243264+sonofkoolaidman@users.noreply.github.com>
2019-04-22 09:03:39 -0500

    added beginnings of database system.

---
+++ app/src/main/java/com/ctech/max/criminalintent/database/CrimeBaseHelper.java
@@ -0,0 +1,8 @@
+package com.ctech.max.criminalintent.database;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CrimeBaseHelper extends SQLiteOpenHelper {
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "crimeBase.db";
+}
---
+++ app/src/main/java/com/ctech/max/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,16 @@
+package com.ctech.max.criminalintent.database;
+
+public class CrimeDbSchema {
+
+    // define the table where we will store the crime data
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+
+        public static final class Columns {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+}
