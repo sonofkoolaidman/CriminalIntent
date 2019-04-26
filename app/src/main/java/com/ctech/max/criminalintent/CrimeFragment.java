b4b052c0a5b71e03f8883779377653d9276d66cb
sonofkoolaidman <35243264+sonofkoolaidman@users.noreply.github.com>
2019-04-26 08:37:29 -0500

    sending Crime Reports works perfectly.

--- app/src/main/java/com/ctech/max/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/ctech/max/criminalintent/CrimeFragment.java
@@ -2,6 +2,9 @@ package com.ctech.max.criminalintent;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
@@ -140,6 +143,12 @@ public class CrimeFragment extends Fragment {
             mSuspectButton.setText(mCrime.getSuspect());
         }
 
+        PackageManager packageManager = getActivity().getPackageManager();
+        if (packageManager.resolveActivity(pickContact,
+                PackageManager.MATCH_DEFAULT_ONLY) == null) {
+            mSuspectButton.setEnabled(false);
+        }
+
         return v;
     }
 
@@ -153,6 +162,29 @@ public class CrimeFragment extends Fragment {
             Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
             mCrime.setDate(date);
             updateDate();
+        } else if (requestCode == REQUEST_CONTACT && data != null) {
+
+            Uri contactUri = data.getData();
+            // specify which fields you want your query to return values for.
+            String[] queryFields = new String[]{
+                    ContactsContract.Contacts.DISPLAY_NAME
+            };
+            // perform your query - the Uri is like a "where" clause here
+            Cursor c = getActivity().getContentResolver()
+                    .query(contactUri, queryFields, null, null, null);
+            try {
+                // double-check that you actually got results
+                if (c.getCount() == 0) {
+                    return;
+                }
+                // pull out the first column of the first row of data - the suspect's name.
+                c.moveToFirst();
+                String suspect = c.getString(0);
+                mCrime.setSuspect(suspect);
+                mSuspectButton.setText(suspect);
+            } finally {
+                c.close();
+            }
         }
     }
 
